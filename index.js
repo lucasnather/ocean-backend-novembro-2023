88824190e5ea0150b4218a65956f3442cb6da0c2
lucasnather <natherlucas@gmail.com>
2023-11-09 14:56:22 +0000

    criação dos endpoints

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!')
 })
@@ -10,6 +12,60 @@ app.get('/oi', (req, res) => {
     res.send('Olá mundo!!!')
 })
 
-app.listen(3000,() => {
+const lista = ["sao paulo", "vasco", "flamengo", "fluminense"]
+
+app.get("/item", (req, res) => {
+    res.send(lista)
+})
+
+app.get("/item/:id", (req, res) => {
+    const { id } = req.params
+    const item = id - 1
+
+    if (!lista[item]) return res.send("não existe")
+
+    res.send(lista[item])
+})
+
+app.post("/item", (req, res) => {
+    const { nome } = req.body
+
+    lista.push(nome)
+
+    res.send(lista)
+})
+
+app.put("/item/:id", (req, res) => {
+    const { id } = req.params
+    const { nome } = req.body
+    const time = id - 1
+
+    if (!lista[time]) return res.send("não existe")
+
+    const findIndex = lista.findIndex((_, index) => {
+        return index === Number(time)
+    })
+
+    lista[findIndex] = nome
+
+    res.send(lista)
+})
+
+app.delete("/item/:id", (req, res) => {
+    const { id } = req.params
+    const time = id - 1
+
+    if (!lista[time]) return res.send("não existe")
+
+    const findIndex = lista.findIndex((_, index) => {
+        return index === Number(time)
+    })
+
+    lista.splice(findIndex, 1)
+
+    res.send(lista)
+})
+
+app.listen(3000, () => {
     console.log("rodou");
 })
